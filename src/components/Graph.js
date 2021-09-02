4f4c1bf05f30c74d97c00f530e8fb4956b09133e
Trace <tracedelange@me.com>
2021-09-02 08:06:42 -0500

    new methods for fetching, new fin-data api without daily call limits

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,10 +1,113 @@
 import React from 'react'
+import { LineChart, Line, CartesianGrid, XAxis, Tooltip, YAxis, ResponsiveContainer } from 'recharts';
+import { ButtonGroup } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Graph = () => {
+
+
+const useStyles = makeStyles((theme) => ({
+    rangebutton: {
+        height: '4vmin',
+        // marginLeft: "10vmin",
+        marginTop: "1vmin"
+    },
+    selectedbutton: {
+        height: '4vmin',
+        // marginLeft: "10vmin",
+        marginTop: "1vmin",
+        color: 'black',
+        fontSize: "2vmin"
+
+    },
+}));
+
+
+const dateSort = (x,y) => {
+    if (Date.parse(x.date) > Date.parse(y.date)){
+        return -1
+    }
+    if (Date.parse(x.date) < Date.parse(y.date)){
+        return 1
+    }
+    else {
+        return 0
+    }
+}
+
+
+const Graph = ({processedDisplayData, ticker_name, selectedTimeFrame, handleRangeClick, processedVantageData}) => {
+
+    const classes = useStyles();
+
+    let combinedArray = []
+
+    // Object.keys(processedVantageData).forEach((item) => {
+    //     // console.log(processedVantageData[item]['date'])
+    //     let vantageDate = processedVantageData[item]['date']
+    //     let found = processedDisplayData.find(item => item['date'] === vantageDate)
+    //     let i = processedDisplayData.indexOf(found)
+
+    //     processedDisplayData[i]['avg'] = processedVantageData[item]['avg']
+
+    // })
+
+    let average = (processedVantageData.reduce((a, b) => ({avg: a.avg + b.avg})))
+
+
+    console.log(average)
+
+    Object.keys(processedDisplayData).forEach((item) => {
+
+        let mentionDate = processedDisplayData[item]['date']
+        let found = processedVantageData.find(item => item['date'] === mentionDate)
+        // let i = processedVantageData.indexOf(found)
+
+        if (found){
+            processedDisplayData[item]['avg'] = found['avg']
+        } else {
+            processedDisplayData[item]['avg'] = (average.avg) / processedVantageData.length
+        }
+            // processedDisplayData[item]['avg'] = (average['avg']/processedVantageData.length)
+
+    })
+
+    // console.log(processedDisplayData)
+    console.log(processedDisplayData)
+    
+    
+
+    // console.log(combinedArray)
+
     return (
-        <div>
+        <>
+            <div className='graph-container'>
+                <div className='break' />
+                <ResponsiveContainer width="90%" height="95%">
+                    <LineChart width={1000} height={300} data={(processedDisplayData.sort(dateSort)).reverse()} margin={{ top: 20, right: 0, bottom: 10, left: 50 }}>
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis minTickGap={80} dataKey="date" tickCount={5} offset={10} />
+                        <YAxis yAxisId="left" />
+                        <YAxis yAxisId="right" orientation="right" />
+                        <Line yAxisId="left" type="monotone" dataKey="estimated_outreach" stroke="#a0e6ff" dot={false} />
+                        <Line yAxisId="right" type="monotone" dataKey="avg" stroke="blue" dot={false} />
+                        <Tooltip />
+                    </LineChart>
+                </ResponsiveContainer>
+
+
+            </div>
+
+            <div className='graph-options-container'>
+                <h3 className='graph-header'>${ticker_name.ticker_name}</h3>
+                <ButtonGroup color="primary" aria-label="outlined primary button group">
+                    <Button className={selectedTimeFrame === 'WEEK' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='week'>Week</Button>
+                    <Button className={selectedTimeFrame === 'MONTH' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='month'>Month</Button>
+                    <Button className={selectedTimeFrame === 'YEAR' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='year'>Year</Button>
+                </ButtonGroup>
+            </div>
             
-        </div>
+        </>
     )
 }
 
--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -4,23 +4,9 @@ import { LineChart, Line, CartesianGrid, XAxis, Tooltip, YAxis, ResponsiveContai
 import spinner from '../assets/loading.gif'
 import { ButtonGroup } from '@material-ui/core';
 import { Button } from '@material-ui/core';
+import Graph from './Graph';
 import { makeStyles } from '@material-ui/core/styles';
-import { object } from 'prop-types';
-import { BackgroundColor } from 'chalk';
 
-
-const dateSort = (x,y) => {
-    if (Date.parse(x.date) > Date.parse(y.date)){
-        return -1
-    }
-    if (Date.parse(x.date) < Date.parse(y.date)){
-        return 1
-    }
-    else {
-        return 0
-    }
-
-}
 
 
 const useStyles = makeStyles((theme) => ({
@@ -40,160 +26,232 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-const Ticker = () => {
+const processData = (cutoffDate, tickerData) => {
 
-    const classes = useStyles();
-    const [tickerData, setTickerData] = useState({})
-    const [dataLoaded, setDataLoaded] = useState(false)
-    const [selectedTimeFrame, setSelectedTimeFrame] = useState('WEEK')
-    const [cutoffDate, setCutoffDate] = useState(null)
-    const [processedDisplayData, setProcessedDisplayData] = useState([])
-    // const [dateArray, setDateArray] = useState([])
-
-    let ticker_name = useParams()
     let today = new Date()
+    let displayData = []
+    let dateArray = []
 
-    
-    useEffect(() => {
-        fetch(`http://localhost:9292/tickers/name/${ticker_name.ticker_name}`)
+    Object.keys(tickerData['mentions_by_recent']).forEach((item) => {
+
+        let mention_date = Date.parse(tickerData['mentions_by_recent'][item]['date'])
+
+        if (mention_date > cutoffDate.getTime()) {
+
+            displayData.push(tickerData['mentions_by_recent'][item])
+
+            let d = new Date(0)
+            d.setUTCSeconds(mention_date / 1000)
+            dateArray.push(d.toISOString().split('T')[0])
+
+        }
+    })
+
+    const start = cutoffDate; //Start Date = cutoff
+    const end = today; //End day = Today
+
+    let loop = new Date(start);
+    while (loop <= end) {
+        if (!dateArray.includes(loop.toISOString().split('T')[0])) {
+            //Date is missing from range, make sure to add it. We might disable all time graph for performance.
+            let fill = {
+                date: loop.toISOString().split('T')[0],
+                estimated_outreach: 0
+            }
+            displayData.push(fill)
+        }
+        let newDate = loop.setDate(loop.getDate() + 1);
+        loop = new Date(newDate);
+    }
+    return displayData
+}
+
+const processVantageData = (vantageData, cutoffDate) => {
+
+    // console.log('cutoff date')
+    console.log(cutoffDate)
+
+    let displayData = []
+    let dateArray = []
+
+    console.log(vantageData)
+
+    Object.keys(vantageData['results']).forEach((item) => {
+
+        let mention_date = vantageData['results'][item]['t']
+        // console.log(vantageData['results'][item]['t'])
+
+        // console.log(mention_date)
+        // console.log(cutoffDate.getTime())
+        // console.log(cutoffDate.getTime() < mention_date/1000)
+
+        if ((mention_date) > cutoffDate.getTime()) {
+
+            let avg = (parseFloat(vantageData['results'][item]['h']) + parseFloat(vantageData['results'][item]['l']))
+
+            let vd = new Date(0); // The 0 there is the key, which sets the date to the epoch
+            vd.setUTCSeconds((vantageData['results'][item]['t']) / 1000);
+
+
+            let displayItem = {
+
+                date: vd.toISOString().split('T')[0],
+                avg: parseFloat(avg.toFixed(2))
+
+            }
+
+            displayData.push(displayItem)
+
+            let d = new Date(0)
+            d.setUTCSeconds(mention_date / 1000)
+            dateArray.push(d.toISOString().split('T')[0])
+
+        }
+    })
+    console.log(displayData)
+    return displayData
+}
+
+const fetchAndReturnDisplayData = (ticker, cutoff, setTickerData) => {
+
+    return fetch(`http://localhost:9292/tickers/name/${ticker}`)
         .then(resp => resp.json())
         .then(data => {
-            // console.log(data)
+
+            // console.log('We should only see this once per reload')
             setTickerData(data)
-            let cutoff = new Date()
-            cutoff.setDate(cutoff.getDate() - 7);
-            setCutoffDate(cutoff)
+            // let cutoff = new Date()
+            // cutoff.setDate(cutoff.getDate() - 7);
 
+            let dataToBeGraphed = processData(cutoff, data)
+            return dataToBeGraphed
+            // setDataLoaded(true)
         })
-        // .fetch(`https:`)
 
-    }, [])
-    
-    const handleRangeClick = (e) => {
+}
 
-        setSelectedTimeFrame(e.target.innerText)
+const fetchAndReturnVantageData = (ticker, cutoff, setVantageTickerData) => {
+
+    let d = new Date()
+    let today = d.toISOString().split('T')[0]
+    let yearAgo = d.setFullYear(d.getFullYear() - 1);
+
+    let ly = new Date(0)
+    ly.setUTCSeconds(yearAgo / 1000)
+
+    return fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${ly.toISOString().split('T')[0]}/${today}?adjusted=true&sort=asc&limit=1000&apiKey=bWEa_uqaWdOXNLpPFHRMgQ3YksGyOatW`)
+    .then(resp => resp.json())
+    .then(data => {
+
+        console.log(data)
+        setVantageTickerData(data) //save the data so when resolution is changed no re-render occurs
+
+
         
+        let toBeGraphed = processVantageData(data, cutoff)
+        console.log(toBeGraphed)
+        return toBeGraphed
+
+
+        // let processedData = processVantageData(cutoff, data)
+
+
+
+    })
+
+}
+
+
+const Ticker = () => {
+
+
+    //Should be responsible for loading and serving the display data to Graph.
+
+    const classes = useStyles();
+
+    const [tickerData, setTickerData] = useState({})
+    const [selfDataLoaded, setSelfDataLoaded] = useState(false)
+
+    const [vantageTickerData, setVantageTickerData] = useState({}) //meant to hold the inital back end call to preserve data and not recall each render
+    const [vantageTickerDataLoaded, setVantageTickerDataLoaded] = useState(false)
+    const [processedVantageData, setProcessedVantageData] = useState([])
+
+    const [selectedTimeFrame, setSelectedTimeFrame] = useState('WEEK')
+    const [processedDisplayData, setProcessedDisplayData] = useState([])
+
+    const ticker_name = useParams()
+    useEffect(() => { //on load, fetch ticker info from the backend, set ticker data on response and set cutoff date to one week in the past.
+
+        let cutoff = new Date()
+        cutoff.setDate(cutoff.getDate() - 7);
+
+        fetchAndReturnDisplayData(ticker_name.ticker_name, cutoff, setTickerData)
+            .then((data) => {
+                // console.log(data)
+                setProcessedDisplayData(data)
+                setSelfDataLoaded(true)
+            })
+
+        fetchAndReturnVantageData(ticker_name.ticker_name, cutoff, setVantageTickerData)
+        .then((data) => {
+            // console.log(data)
+            setProcessedVantageData(data)
+            setVantageTickerDataLoaded(true)
+        })
+    }, [])
+
+
+    const handleRangeClick = (e) => {
+        setSelectedTimeFrame(e.target.innerText)
     }
 
 
     useEffect(() => {
         // let today = new Date()
+        console.log('UseEffect Activated Via TimeFrameSelection Changing')
+        if (selfDataLoaded) {
+            if (selectedTimeFrame === 'WEEK') {
+                let cutoff = new Date()
+                cutoff.setDate(cutoff.getDate() - 7);
+    
 
-        if (selectedTimeFrame === 'WEEK') {
-            let cutoff = new Date()
-            cutoff.setDate(cutoff.getDate() - 7);
-            setCutoffDate(cutoff)
-            console.log(cutoff)
-        } else if (selectedTimeFrame === "MONTH") {
-            let cutoff = new Date()
-            cutoff.setMonth(cutoff.getMonth() - 1);
-            setCutoffDate(cutoff)
-        } else if (selectedTimeFrame === "YEAR"){
-            let cutoff = new Date()
-            cutoff.setFullYear(cutoff.getFullYear() - 1);
-            setCutoffDate(cutoff)
+                setProcessedDisplayData(processData(cutoff, tickerData))
+                setProcessedVantageData(processVantageData(vantageTickerData, cutoff))
+                
+                
+            } else if (selectedTimeFrame === "MONTH") {
+                let cutoff = new Date()
+                cutoff.setMonth(cutoff.getMonth() - 1);
+                
+                setProcessedDisplayData(processData(cutoff, tickerData))
+                setProcessedVantageData(processVantageData(vantageTickerData, cutoff))
+                // let dataToBeGraphed = processData(cutoff, tickerData)
+                // setProcessedDisplayData(dataToBeGraphed)
+    
+            } else if (selectedTimeFrame === "YEAR") {
+                let cutoff = new Date()
+                cutoff.setFullYear(cutoff.getFullYear() - 1);
+                
+                setProcessedDisplayData(processData(cutoff, tickerData))
+                setProcessedVantageData(processVantageData(vantageTickerData, cutoff))
+            }
         }
     }, [selectedTimeFrame])
 
-    useEffect(() => {
-
-        let today = new Date()
-        console.log(cutoffDate)
-
-        let displayData = []
-        let dateArray = []
-        console.log(tickerData)
-        if (dataLoaded === true) {
-            Object.keys(tickerData['mentions_by_recent']).forEach((item) => {
-                
-                let mention_date = Date.parse(tickerData['mentions_by_recent'][item]['date'])
-    
-                if (mention_date > cutoffDate.getTime()) {
-    
-                    displayData.push(tickerData['mentions_by_recent'][item])
-    
-                    let d = new Date(0)
-                    d.setUTCSeconds(mention_date / 1000)
-                    dateArray.push(d.toISOString().split('T')[0])
-
-                }
-            })
-
-            const start = cutoffDate; //Start Date = cutoff
-            const end = today; //End day = Today
-            
-            console.log(dateArray)
-
-            let loop = new Date(start);
-            while (loop <= end) {
-
-                if (!dateArray.includes(loop.toISOString().split('T')[0])) {
-
-                    //Date is missing from range, make sure to add it. We might disable all time graph for performance.
-                    let fill = {
-                        date: loop.toISOString().split('T')[0],
-                        estimated_outreach: 0
-                    }
-                    displayData.push(fill)
-                    
-                }
-    
-                let newDate = loop.setDate(loop.getDate() + 1);
-                loop = new Date(newDate);
-            }
-    
-        }
 
 
-        console.log(displayData)
-        setProcessedDisplayData(displayData)
-        
-    }, [cutoffDate])
-    
-
-    useEffect(()=> {
-        setTimeout(() => {
-            if (processedDisplayData) {
-                setDataLoaded(true)
-            }
-            }, 100)
-    }, [tickerData])
-
-
-    console.log(selectedTimeFrame)
+    // console.log(processedDisplayData)
 
     return (
         <div>
-            {dataLoaded ?
-                <>
-
-                    <div className='graph-container'>
-                        <div className='break' />
-                        <ResponsiveContainer width="90%" height="95%">
-                            <LineChart width={1000} height={300} data={(processedDisplayData.sort(dateSort)).reverse()} margin={{ top: 20, right: 0, bottom: 10, left: 50 }}>
-                                <Line type="monotone" dataKey="estimated_outreach" stroke="#8884d8" dot={false} />
-                                <CartesianGrid stroke="#ccc" />
-                                <Tooltip />
-                                <XAxis minTickGap={100} dataKey="date" tickCount={5} offset={10} />
-                                <YAxis startOffset={100} />
-                            </LineChart>
-                        </ResponsiveContainer>
-
-
-                    </div>
-
-                    <div className='graph-options-container'>
-                        <h3 className='graph-header'>${ticker_name.ticker_name}</h3>
-                        <ButtonGroup color="primary" aria-label="outlined primary button group">
-                            <Button className={selectedTimeFrame === 'WEEK' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='week'>Week</Button>
-                            <Button className={selectedTimeFrame === 'MONTH' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='month'>Month</Button>
-                            <Button className={selectedTimeFrame === 'YEAR' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='year'>Year</Button>
-                            {/* <Button className={selectedTimeFrame === 'ALL TIME' ? classes.selectedbutton : classes.rangebutton} onClick={handleRangeClick} name='all'>All Time</Button> */}
-                        </ButtonGroup>
-
-                    </div>
-
-                </>
+            {selfDataLoaded ?
+                <Graph
+                    ticker_name={ticker_name}
+                    processedDisplayData={processedDisplayData}
+                    processedVantageData={processedVantageData}
+                    selectedTimeFrame={selectedTimeFrame}
+                    handleRangeClick={handleRangeClick}
+                />
                 : <img className='loading-spinner' src={spinner} alt='loading spinner'></img>}
         </div >
     )
